8e6863c0fe5136033969c273e55004a95633e985
Thiago Silva <thiago25pro@gmail.com>
2021-07-19 23:01:23 -0300

    last commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Digital Innovation One - Bootcamp GFT START
+
+APP simples de cadastro de séries em .NET.
--- classes/EntidadeBase.cs
+++ classes/EntidadeBase.cs
@@ -1,7 +1,6 @@
 namespace DIO.Series
 {
-    public abstract class EntidadeBase
-    {
-        public int Id { get; protected set; }
+    public abstract class EntidadeBase  {
+      public int Id { get; protected set; }
     }
 }
